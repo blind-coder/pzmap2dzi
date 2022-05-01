ffa5f9cfadf5a7371e95bb3aeb4522e38f1f1f4f
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2022-05-01 20:47:47 +0200

    cleanup

--- scripts/copy_html.bat
+++
@@ -1,4 +0,0 @@
-pushd %~dp0..
-set /p out_path= <out_path.txt
-xcopy /Q /E /I /Y html "%out_path%\html"
-popd
--- scripts/unpack_texture.bat
+++
@@ -1,6 +0,0 @@
-pushd %~dp0..
-set /p out_path= <out_path.txt
-set /p pz_path= <pz_path.txt
-python -m pzmap2dzi.texture -o "%out_path%/texture" -m 16 "%pz_path%"
-popd
-
