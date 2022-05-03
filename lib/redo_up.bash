e7e59222aeba6ceeff844a9862a1944ed18d49b6
Benjamin Schieder <benjamin.schieder@nasdaq.com>
2022-05-03 08:00:20 +0200

    add helper scripts

---
+++ lib/redo_up.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -u
+
+base="${1}"
+level="${2}"
+xmin="${3}"
+xmax="${3}"
+ymin="${4}"
+ymax="${4}"
+while [ -d "${base}/${level}" ]; do
+	for x in $(seq ${xmin} ${xmax}); do
+		for y in $(seq ${ymin} ${ymax}); do
+			rm -v ${base}/${level}/${x}_${y}.png
+		done
+	done
+	xmin=$((xmin*2))
+	xmax=$((xmax*2+1))
+	ymin=$((ymin*2))
+	ymax=$((ymax*2+1))
+	level=$((level-1))
+done
